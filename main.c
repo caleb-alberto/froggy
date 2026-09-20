eeac2afd00c2f6083074a63a0fa4ee35eeace2eb
caleb-alberto <calberto3418@gmail.com>
2026-09-20 16:03:48 -0700

    refactored structure and added reading rom and opcodes

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <time.h>
 
 
 const unsigned int START_ADDRESS = 0x200;
+const unsigned int FONTSET_SIZE = 80;
+const unsigned int FONTSET_START_ADDRESS = 0x50;
 
 typedef struct {
 	uint8_t registers[16];
@@ -17,24 +20,32 @@ typedef struct {
 	uint8_t keypad[16];
 	uint32_t video[64 * 32];
 	uint16_t opcode;
-} Chip8;
+} chip8;
 
-void main_loop() {
-}
+uint8_t fontset[FONTSET_SIZE] =
+{
+	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
 
-void read_ROM(Chip8* this, char* filename) {
-        FILE* rom = fopen(filename, "rb");
-
-        fseek(rom, 0, SEEK_END);
-        long size = ftell(rom);
-        uint8_t buffer[size];
-
-        fseek(rom, 0, SEEK_SET);
-        fread(buffer, sizeof(uint8_t), size, rom);
-
-	for (long i = 0; i < size; ++i)
-		this->memory[START_ADDRESS + i] = buffer[i];
-}
+void main_loop(chip8* this);
+uint16_t get_instruction(chip8* this);
+void read_ROM(chip8* this, char* filenaA1);
+uint8_t rand_byte();
 
 int main() {
         struct timespec start, end;
@@ -42,8 +53,16 @@ int main() {
 
         time_t time_s = 0;
         long time_n = 0, prev_time_n = 0, last_cycle = 0;
-        int loops = 0;
         const long freq_ns = (long)(1e9 / 700);
+
+        chip8 mychip8;
+        mychip8.pc = START_ADDRESS;
+
+        for (int i = 0; i < FONTSET_SIZE; i++)
+                mychip8.memory[FONTSET_SIZE + i] = fontset[i];
+
+        mychip8.memory[mychip8.pc] = 0x65;
+        mychip8.memory[mychip8.pc+1] = 0xA1;
 
         while (1) {
 		clock_gettime(CLOCK_MONOTONIC, &end);
@@ -51,7 +70,6 @@ int main() {
 		time_n = end.tv_nsec - start.tv_nsec;
 
                 if (last_cycle > freq_ns) {
-                        loops++;
                         last_cycle = 0;
                 }
                 else {
@@ -66,6 +84,50 @@ int main() {
                 if (time_n >= 0 && time_n <= 100 && time_s == 1)
                         break;
         }
+        main_loop(&mychip8);
+        printf("pc after single loop %X\n", mychip8.pc);
+}
 
-        printf("%d loops in %d seconds\n", loops, (int)time_s);
+void main_loop(chip8* this) {
+        this->opcode = get_instruction(this);
+
+        uint8_t first = (this->opcode & 0xF000) >> 12;
+        uint8_t x = (this->opcode & 0x0F00) >> 8;
+        uint8_t y = (this->opcode & 0x00F0) >> 4;
+        uint8_t n = (this->opcode & 0x000F);
+        uint8_t nn = (this->opcode & 0x00FF);
+        uint16_t nnn = (this->opcode & 0x0FFF);
+
+        //switch
+}
+
+uint16_t get_instruction(chip8* this) {
+        uint16_t instruction = (this->memory[this->pc] & 0xFF) << 8;
+        this->pc++;
+
+        instruction = (instruction & 0xFF00) ^ (this->memory[this->pc] & 0xFF);
+        this->pc++;
+
+        return instruction;
+}
+
+void read_ROM(chip8* this, char* filename) {
+        FILE* rom = fopen(filename, "rb");
+
+        fseek(rom, 0, SEEK_END);
+        long size = ftell(rom);
+        uint8_t buffer[size];
+
+        fseek(rom, 0, SEEK_SET);
+        fread(buffer, sizeof(uint8_t), size, rom);
+
+	for (long i = 0; i < size; ++i)
+		this->memory[START_ADDRESS + i] = buffer[i];
+}
+
+uint8_t rand_byte() {
+        srand(time(NULL));
+        uint8_t byte = (uint8_t)rand();
+
+        return byte;
 }
