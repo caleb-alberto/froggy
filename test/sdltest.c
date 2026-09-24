faad5357a614856753cc900d810ce3d72e518899
Caleb-Alberto <calberto3418@gmail.com>
2026-09-23 22:28:48 -0700

    figured out how to create and render pixels to window in test dir with SDL

--- test/sdltest.c
+++ test/sdltest.c
@@ -1,15 +1,48 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+#define width 64
+#define height 32
+#define scale 20
 
 int main() {
 	SDL_Window *window;
 
 	SDL_Init(SDL_INIT_VIDEO);
-	printf("SDL initialized\n");
 
-	window = SDL_CreateWindow("An SDL2 window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
-        while (1) {}
+        uint32_t video[width * height];
+        memset(video, 0, sizeof(video));
 
-	SDL_Quit();
+	window = SDL_CreateWindow(
+                "froggy",
+                SDL_WINDOWPOS_UNDEFINED,
+                SDL_WINDOWPOS_UNDEFINED,
+                width * scale,
+                height * scale,
+                0
+        );
+
+        SDL_Renderer* renderer = SDL_CreateRenderer(window, 0, 0);
+        SDL_Texture* texture = SDL_CreateTexture(
+                renderer,
+                SDL_PIXELFORMAT_ARGB8888,
+                SDL_TEXTUREACCESS_STREAMING,
+                width,
+                height
+        );
+
+        SDL_UpdateTexture(texture, NULL, video, width * sizeof(uint32_t));
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+
+        SDL_Event event;
+        while (1) {
+                SDL_PollEvent(&event);
+                if (event.type == SDL_QUIT) {
+                        break;
+                }
+        }
+
 	return 0;
 }
